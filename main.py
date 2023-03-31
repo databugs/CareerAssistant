0b38e76674fdc21dc13258dbe38aed06b9568320
databugs <41240594+databugs@users.noreply.github.com>
2023-03-31 17:20:29 +0100

    add logging

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import logging
+import json
+
 from fastapi import FastAPI, Request, Response, HTTPException, status
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
 
 @app.post('/', status_code=status.HTTP_202_ACCEPTED)
 async def telegram_webhook(request: Request):
@@ -8,6 +13,7 @@ async def telegram_webhook(request: Request):
     if not payload:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty payload")
     json_payload = payload.decode('utf-8')
-    print(json_payload)
-    print(type(json_payload))
+    json_payload = json.loads(json_payload)
+    logging.info(f"Received JSON payload: {json_payload}")
+    logging.info(f"Type of JSON payload: {type(json_payload)}")
     return Response(status_code=status.HTTP_202_ACCEPTED)
