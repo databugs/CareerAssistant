adc0525810f7a5ed917ae72431bbdbf471a4ef33
databugs <41240594+databugs@users.noreply.github.com>
2023-03-31 15:43:11 +0100

    add requirements

--- update.py
+++ update.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 @app.post('/', status_code=HTTPStatus.ACCEPTED)
 async def telegram_webhook(request: Response):
-    payload = request.json()
+    payload = await request.json()
     print(payload)
     return payload
