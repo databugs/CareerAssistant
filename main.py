cca7c59859b00c08aa2e1ff9405fd741dae1c792
databugs <41240594+databugs@users.noreply.github.com>
2023-03-31 17:46:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import json
+import os
 
 from fastapi import FastAPI, Request, Response, HTTPException, status
 
@@ -6,7 +8,7 @@ from telegram import Update
 from telegram.ext import (ApplicationBuilder, CommandHandler,
                           ConversationHandler, MessageHandler, filters, ContextTypes
                           )
-import os
+
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
@@ -23,7 +25,8 @@ async def telegram_webhook(request: Request):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty payload")
     json_payload = payload.decode('utf-8')
     try:
-        update = Update.de_json(json_payload, bot)
+        update_dict = json.loads(json_payload)
+        update = Update.de_json(update_dict, bot)
         logging.info(f"Received Telegram update: {update}")
         logging.info(f"Type of Telegram Update: {type(update)}")
     except Exception as e:
