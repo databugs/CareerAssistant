3240289ca10b4220c7ca0fa7e06839ef66f14871
databugs <41240594+databugs@users.noreply.github.com>
2023-03-31 17:13:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Response
-from http import HTTPStatus
+from fastapi import FastAPI, Request, Response, HTTPException, status
 
 app = FastAPI()
 
-@app.post('/', status_code=HTTPStatus.ACCEPTED)
-async def telegram_webhook(request):
-    payload = await request
-    print(payload)
-    print(type(payload))
-    return payload
+@app.post('/', status_code=status.HTTP_202_ACCEPTED)
+async def telegram_webhook(request: Request):
+    payload = await request.body()
+    if not payload:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty payload")
+    json_payload = payload.decode('utf-8')
+    print(json_payload)
+    print(type(json_payload))
+    return Response(status_code=status.HTTP_202_ACCEPTED)
